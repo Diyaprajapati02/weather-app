0df23bcf93ddfcbf1d3a625cebcbcc617812d216
Diyaprajapati02 <diyap971@gmail.com>
2026-04-09 14:53:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+async function getWeather()
+{
+    let city= document.getElementById("city").value;
+    let apikey= "YOUR_API_KEY";
+    let url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+    let response= await fetch(url);
+    let data= await response.json();
+
+    if (data.cod=== 404)
+    {
+        document.getElementById("result").innerHTML= "City not found ❌";
+        return;
+    }
+
+    document.getElementById("result").innerHTML= `
+    <h2>${data.name}</h2>
+    <p>🌡 Temperature: ${data.main.temp} °C </p>
+    <p>☁ Weather: ${data.weather[0].main} </p>
+    <p>💧 Humidity: ${data.main.humidity}% </p>
+    `;
+
+}
